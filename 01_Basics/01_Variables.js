6c085dcc7e0e49c9827565e5739a4823151e59b2
Atulit95 <atulitgupta436@gmail.com>
2024-05-02 01:23:45 +0530

    day1

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,19 @@
+const accountId=12321
+let accountEmail="xyz@gmail.com"
+var accountPassword="34528"
+accountCity="Kuapur"
+let accountState
+
+// accountId=5  //Not Allowed
+console.log(accountId);
+
+/*
+prefer not to use 
+var because of issue of block and funtional scope.
+*/
+
+accountEmail="vds@gmail.com"
+accountPassword="234323"
+accountCity="benguluru"
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello")  // can not be used in node while in browser can
+
+//-------Primitive Datatype---------
+
+let name="Atulit" //String
+let age=18        //number
+let loggedIn=false //boolean
+
+//'null'        is standalone value
+//'undefined'   when variable is not defined
+//symbol        used to define unique value
+
+//Object
