b7a8bc0c96f5e9af8ef39bb0357615d76844ee40
Atulit95 <atulitgupta436@gmail.com>
2024-05-02 07:09:09 +0530

    day2

---
+++ 01_Basics/03_ConversionOperation.js
@@ -0,0 +1,15 @@
+let score="45"
+
+console.log(typeof(score));
+console.log(typeof score);
+
+let valueInNumber=Number(score);
+console.log(typeof valueInNumber)
+
+//if score==null it will return 0
+//and if value cannnot be converted than it will return NaN 
+// for Boolean
+    // 1=> true;  0=>false
+    // ""=>false;  "Atulit"=>true
+
+console.log(Number("77gha"))
